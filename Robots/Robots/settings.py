67a90b4e0360626412de2d02a06c3c690b674f5c
sirotkin.nikola@mail.ru <sirotkin.nikola@mail.ru>
2024-01-11 01:17:08 +0300

    docker ready and tested

--- Robots/Robots/settings.py
+++ Robots/Robots/settings.py
@@ -142,6 +142,6 @@ AUTH_USER_MODEL = 'RobotsAPI.User'
 CELERY_TIMEZONE = "Europe/Moscow"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
-CELERY_BROKER_URL = "redis://localhost:6379"
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 
